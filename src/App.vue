17b66e7a43018c101bda784dc5d392ff6c5f4701
peerlessice <52813006+peerlessice@users.noreply.github.com>
2021-10-29 10:14:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" width="90%">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
